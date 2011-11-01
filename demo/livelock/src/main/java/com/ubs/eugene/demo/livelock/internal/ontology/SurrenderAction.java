25d9e9c781bee9b74632bdd9a540d9440faab625
Jakub Kozlowski <mail@jakub-kozlowski.com>
2011-11-01 23:04:26 +0000

    [feature] Implemented livelock simulation. * Implemented ResourceOntology to handle complex conversations. * Implemented ResourceServer that uses OntologyServer to respond   to RequestAction and SurrenderAction. * Implemented WorkerAgent to send actions using SimpleAchieveREInitiator;   The WorkerAgent itself is a Finite State Machine.

---
+++ demo/livelock/src/main/java/com/ubs/eugene/demo/livelock/internal/ontology/SurrenderAction.java
@@ -0,0 +1,32 @@
+package com.ubs.eugene.demo.livelock.internal.ontology;
+
+import jade.content.AgentAction;
+
+/**
+ * Represents an action of surrendering a {@link Resource} in {@link ResourceOntology}.
+ *
+ * @author Jakub D Kozlowski
+ * @since 0.1.3
+ */
+public class SurrenderAction implements AgentAction {
+
+    private Resource resource;
+
+    /**
+     * Gets the resource for this <code>SurrenderAction</code>.
+     *
+     * @return the resource.
+     */
+    public Resource getResource() {
+        return this.resource;
+    }
+
+    /**
+     * Sets the resource for this <code>SurrenderAction</code>.
+     *
+     * @param resource new resource.
+     */
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+}
