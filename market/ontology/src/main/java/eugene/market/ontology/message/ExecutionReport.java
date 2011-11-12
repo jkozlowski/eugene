0db6cc967d38cdc8f292b97602188b269c8fb67e
Jakub Kozlowski <mail@jakub-kozlowski.com>
2011-11-12 19:07:59 +0000

    * ClOrdID, OrderID and started writing Message classes.

---
+++ market/ontology/src/main/java/eugene/market/ontology/message/ExecutionReport.java
@@ -0,0 +1,11 @@
+package eugene.market.ontology.message;
+
+import jade.content.onto.annotations.Element;
+
+/**
+ * @author Jakub D Kozlowski
+ */
+@Element(name = "8")
+public class ExecutionReport {
+
+}
