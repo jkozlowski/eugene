4e7c03a99ca4d88ebad312284dfc3362a72cda03
Jakub Kozlowski <mail@jakub-kozlowski.com>
2011-11-15 14:28:40 +0000

    * Removed redundant Message methods. * Trying to setup unit tests.

---
+++ market/ontology/src/test/java/eugene.market.ontology/NewOrderSingleTest.java
@@ -0,0 +1,15 @@
+package eugene.market.ontology;
+
+import eugene.market.ontology.message.NewOrderSingle;
+
+/**
+ * Tests {@link NewOrderSingle}.
+ *
+ * @author Jakub D Kozlowski
+ * @since 0.2
+ */
+public class NewOrderSingleTest {
+
+
+
+}
