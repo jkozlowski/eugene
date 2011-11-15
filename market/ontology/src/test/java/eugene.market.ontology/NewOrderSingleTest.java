8b986e2ca5813dc0ab3e30b4846042a8b234f035
Jakub Kozlowski <mail@jakub-kozlowski.com>
2011-11-15 18:39:50 +0000

    * First configuration of tests that works.

--- market/ontology/src/test/java/eugene.market.ontology/NewOrderSingleTest.java
+++ market/ontology/src/test/java/eugene.market.ontology/NewOrderSingleTest.java
@@ -1,6 +1,16 @@
 package eugene.market.ontology;
 
 import eugene.market.ontology.message.NewOrderSingle;
+import jade.osgi.service.runtime.JadeRuntimeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.player.Player;
+import org.ops4j.pax.exam.testforge.WaitForService;
+import org.osgi.framework.BundleContext;
+
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
 
 /**
  * Tests {@link NewOrderSingle}.
@@ -10,6 +20,28 @@ import eugene.market.ontology.message.NewOrderSingle;
  */
 public class NewOrderSingleTest {
 
+    @Inject
+    private BundleContext bundleContext;
+
+    @Before
+    public void setup() throws Exception {
+
+        new Player().with(
+                options(
+                        mavenBundle().groupId("jade").artifactId("jade-osgi").versionAsInProject().start()
+                )
+        )
+        .test(WaitForService.class, JadeRuntimeService.class.getName(), 5000)
+        .play();
+
+        System.out.println("Got the bundle: " + bundleContext);
+        System.out.println("Setup a container");
+    }
+
+    @Test
+    public void test() {
+
+    }
 
 
 }
