da4ba555a858620bf31638be04eb21b0d86b7745
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-01-17 20:19:14 +0000

    * Added agent module. * Aded agent/random module.

---
+++ agent/random/src/main/java/eugene/agent/random/RandomTraderAgent.java
@@ -0,0 +1,17 @@
+package eugene.agent.random;
+
+import jade.core.Agent;
+
+/**
+ * Implements the Random Trader Agent.
+ *
+ * @author Jakub D Kozlowski
+ * @since 0.5
+ */
+public class RandomTraderAgent extends Agent {
+
+    @Override
+    public void setup() {
+
+    }
+}
---
+++ agent/random/src/test/java/eugene/agent/random/RandomTraderAgentTest.java
@@ -0,0 +1,10 @@
+package eugene.agent.random;
+
+/**
+ * Tests {@link RandomTraderAgent}.
+ *
+ * @author Jakub D Kozlowski
+ * @since 0.5
+ */
+public class RandomTraderAgentTest {
+}
