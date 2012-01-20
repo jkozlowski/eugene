4c3d915035293970395262333964abbd212ce5df
Jakub Kozlowski <mail@jakub-kozlowski.com>
2012-01-20 11:05:28 +0000

    * Defined a first version of the OrderPlacingBehaviour. Now I need a way to send the orders.

--- agent/noise/src/main/java/eugene/agent/random/impl/behaviour/PlaceOrderBehaviour.java
+++ agent/noise/src/main/java/eugene/agent/random/impl/behaviour/PlaceOrderBehaviour.java
@@ -2,6 +2,11 @@ package eugene.agent.random.impl.behaviour;
 
 import eugene.market.book.OrderBook;
 import eugene.market.client.api.Session;
+import eugene.market.ontology.field.OrderQty;
+import eugene.market.ontology.field.Price;
+import eugene.market.ontology.field.enums.OrdType;
+import eugene.market.ontology.field.enums.Side;
+import eugene.market.ontology.message.NewOrderSingle;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 
@@ -10,22 +15,51 @@ import java.util.Random;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
- * Places
+ * Places either {@link Strategy#AGGRESSIVE}, {@link Strategy#MID} or {@link Strategy#PASSIVE} order for a random
+ * {@link OrderQty} and to a random {@link Side}, and sleeps for a random period of time.
  *
  * @author Jakub D Kozlowski
  * @since 0.5
  */
 public class PlaceOrderBehaviour extends TickerBehaviour {
 
+    public static final int MAX_SPREAD = 10;
+
+    public static final int MIN_SPREAD = 1;
+
+    public static final double DEFAULT_TICK = 1.0D;
+
+    public static final double DEFAULT_CURRENT_PRICE = 100.0D;
+
     public static final Integer MAX_SLEEP = 6000;
 
-    public static final Integer MIN_SLEEP = 1000;
+    public static final int MAX_ORDER_QTY = 3000;
 
-    private final Random random = new Random();
+    public static final int MIN_ORDER_QTY = 1000;
+
+    private final Random generator = new Random();
 
     private final OrderBook orderBook;
-    
+
     private final Session session;
+
+    private enum Strategy {
+
+        /**
+         * Sends a {@link OrdType#LIMIT} order with a price that is one tick above the current best price.
+         */
+        AGGRESSIVE,
+
+        /**
+         * Sends a {@link OrdType#LIMIT} order at the current best price.
+         */
+        MID,
+
+        /**
+         * Sends a {@link OrdType#LIMIT} order a random number ticks away from the current best price.
+         */
+        PASSIVE
+    }
 
     /**
      * Constructs a {@link PlaceOrderBehaviour} that will be executed by this <code>agent</code>
@@ -44,6 +78,81 @@ public class PlaceOrderBehaviour extends TickerBehaviour {
 
     @Override
     protected void onTick() {
-        super.reset();
+
+        final Strategy strategy = getStrategy();
+        final Side side = (0 == generator.nextInt(1)) ? Side.BUY : Side.SELL;
+        final Long orderQty = getOrderQty();
+        final int sleep = generator.nextInt(MAX_SLEEP);
+
+        super.reset(sleep);
+    }
+
+    /**
+     * Gets a random {@link Strategy}.
+     *
+     * @return random {@link Strategy}.
+     */
+    private Strategy getStrategy() {
+        final int random = generator.nextInt(2);
+        return 0 == random ? Strategy.AGGRESSIVE : (1 == random ? Strategy.MID : Strategy.PASSIVE);
+    }
+
+    /**
+     * Gets a random orderQty between {@link PlaceOrderBehaviour#MIN_ORDER_QTY} and {@link
+     * PlaceOrderBehaviour#MAX_ORDER_QTY}.
+     *
+     * @return random orderQty.
+     */
+    private Long getOrderQty() {
+        final int additive = generator.nextInt(MAX_ORDER_QTY - MIN_ORDER_QTY);
+        return new Long(MIN_ORDER_QTY + additive);
+    }
+
+    /**
+     * Gets a {@link NewOrderSingle} that will create an order for this <code>strategy</code>,
+     * <code>side</code> and <code>orderQty</code>.
+     *
+     * @param strategy type of order to send.
+     * @param side     {@link Side} of the order.
+     * @param orderQty quantity of the order.
+     *
+     * @return {@link NewOrderSingle} that will create an order for these parameters.
+     */
+    private NewOrderSingle newOrderSingle(final Strategy strategy, final Side side, Long orderQty) {
+
+        final NewOrderSingle newOrderSingle = new NewOrderSingle();
+        newOrderSingle.setOrderQty(new OrderQty(orderQty));
+        newOrderSingle.setSide(side.field());
+        newOrderSingle.setOrdType(OrdType.LIMIT.field());
+
+        final double curPrice = orderBook.isEmpty(side) ? DEFAULT_CURRENT_PRICE : orderBook.peek(side).getPrice();
+
+        switch (strategy) {
+
+            case AGGRESSIVE:
+                if (side.isBuy()) {
+                    newOrderSingle.setPrice(new Price(curPrice + DEFAULT_TICK));
+                }
+                else {
+                    newOrderSingle.setPrice(new Price(curPrice - DEFAULT_TICK));
+                }
+                break;
+
+            case MID:
+                newOrderSingle.setPrice(new Price(curPrice));
+                break;
+
+            case PASSIVE:
+                final int additive = generator.nextInt(MAX_SPREAD - MIN_SPREAD);
+                if (side.isBuy()) {
+                    newOrderSingle.setPrice(new Price(curPrice - additive));
+                }
+                else {
+                    newOrderSingle.setPrice(new Price(curPrice + additive));
+                }
+                break;
+        }
+
+        return newOrderSingle;
     }
 }
